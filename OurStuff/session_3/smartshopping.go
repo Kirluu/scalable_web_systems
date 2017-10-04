252c17299e401eb7bdbe51757b6d435d5d6a956e
Kirluu <kirluu4thewin@gmail.com>
2017-10-04 10:52:09 +0200

    trash files?

--- OurStuff/session_3/smartshopping.go
+++ OurStuff/session_3/smartshopping.go
@@ -39,6 +39,7 @@ func init() {
 	http.HandleFunc("/complete", completeHandler)
 	http.HandleFunc("/incomplete", incompleteHandler)
 	http.HandleFunc("/goget", getHandler)
+	http.HandleFunc("/query", queryHandler)
 }
 
 
@@ -63,10 +64,10 @@ func completeHandler(w http.ResponseWriter, r *http.Request) {
 	// create a new App Engine context from the HTTP request.
 	ctx := appengine.NewContext(r)
 
-	p := &Item{Name: "gopher", SuperMarket: "Netto"}
+	p := &Item{Name: "gopher", SuperMarket: "Netto", Price: 5}
 
 	// create a new complete key of kind Person and value gopher.
-	key := datastore.NewKey(ctx, "Person", "gopher", 0, nil)
+	key := datastore.NewKey(ctx, "Item", "gopher", 0, nil)
 	// put p in the datastore.
 	key, err := datastore.Put(ctx, key, p)
 	if err != nil {
@@ -81,10 +82,10 @@ func incompleteHandler(w http.ResponseWriter, r *http.Request) {
 	// create a new App Engine context from the HTTP request.
 	ctx := appengine.NewContext(r)
 
-	p := &Item{Name: "gopher", SuperMarket: "Netto"}
+	p := &Item{Name: "gopher", SuperMarket: "Netto", Price: 5}
 
 	// create a new complete key of kind Person.
-	key := datastore.NewIncompleteKey(ctx, "Person", nil)
+	key := datastore.NewIncompleteKey(ctx, "Item", nil)
 	// put p in the datastore.
 	key, err := datastore.Put(ctx, key, p)
 	if err != nil {
@@ -98,18 +99,39 @@ func incompleteHandler(w http.ResponseWriter, r *http.Request) {
 func getHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 
-	key := datastore.NewKey(ctx, "Person", "gopher", 0, nil)
+	key := datastore.NewKey(ctx, "Item", "gopher", 0, nil)
 
 	var p Item
 	err := datastore.Get(ctx, key, &p)
 	if err != nil {
-		http.Error(w, "Person not found", http.StatusNotFound)
+		http.Error(w, "Item not found", http.StatusNotFound)
 		return
 	}
 	fmt.Fprintln(w, p)
 }
 
-var items = []Item{}
+func queryHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
+	var p []Item
+
+	// create a new query on the kind Person
+	q := datastore.NewQuery("Item")
+
+	// select only values where field Age is 10 or lower
+	q = q.Filter("Price =", 0)
+
+	// order all the values by the Name field
+	q = q.Order("Name")
+
+	// and finally execute the query retrieving all values into p.
+	_, err := q.GetAll(ctx, &p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, p)
+}
 
 type Item struct {
 	Name     string `json:"name"`
@@ -120,50 +142,90 @@ type Item struct {
 func addItem(w http.ResponseWriter, r *http.Request){
 	var i Item
 
+	// PARSE JSON
 	dec := json.NewDecoder(r.Body)
+	if dec == nil {
+		fmt.Fprintf(w, "json decoder failed to be created, given body")
+	}
 	err := dec.Decode(&i)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Fprintf(w, "decoding resulted in an error! Check your format!")
+		//http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	items = append(items, i)
-	fmt.Fprintf(w, "Added %v", i.Name)
-}
-
-
-func remove(s []Item, i int) []Item {
-	s[len(s)-1], s[i] = s[i], s[len(s)-1]
-	return s[:len(s)-1]
+	// PUT IN DB
+	ctx := appengine.NewContext(r)
+	key := datastore.NewIncompleteKey(ctx, "Item", nil)
+	key, putErr := datastore.Put(ctx, key, &i)
+	if putErr != nil {
+		fmt.Fprintf(w, "Put failed!")
+		http.Error(w, putErr.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "gopher stored with key %v", key)
 }
 
 func removeItemByName(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "could not read body: %v", err)
+	b, readErr := ioutil.ReadAll(r.Body)
+	if readErr != nil {
+		fmt.Fprintf(w, "could not read body: %v", readErr)
 		return
 	}
 	name := string(b)
 
+	ctx := appengine.NewContext(r)
 
-	for index, e := range items  {
-		if(e.Name == name){
-			items = remove(items, index)
-		}
+	var keyArray []*datastore.Key
+	_, err := datastore.NewQuery("Item").Filter("Name =", name).KeysOnly().GetAll(ctx, &keyArray)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
+	deleteErr := datastore.DeleteMulti(ctx, keyArray)
+	if deleteErr != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func removeAllItems(w http.ResponseWriter, r *http.Request){
-	items = []Item{}
+	ctx := appengine.NewContext(r)
+
+	var keyArray []*datastore.Key
+	_, err := datastore.NewQuery("Item").KeysOnly().GetAll(ctx, &keyArray)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	deleteErr := datastore.DeleteMulti(ctx, keyArray)
+	if deleteErr != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func getItemsTotalPrice(w http.ResponseWriter, r *http.Request) {
-	sum := 0
-	for _, e := range items  {
-		sum += e.Price
+	ctx := appengine.NewContext(r)
+
+	var p []Item
+
+	// create a new query on the kind Person
+	q := datastore.NewQuery("Item")
+
+	// and finally execute the query retrieving all values into p.
+	_, err := q.GetAll(ctx, &p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
+	sum := 0
+	for _, e := range p  {
+		sum += e.Price
+	}
 	fmt.Fprintf(w, "total price: %v", sum)
 }
 
@@ -175,13 +237,19 @@ func getItemsInSupermarket(w http.ResponseWriter, r *http.Request) {
 	}
 	smarket := string(b)
 
+	ctx := appengine.NewContext(r)
 
-	sum := []Item{}
-	for _, e := range items  {
-		if(e.SuperMarket == smarket){
-			sum = append(sum, e)
-		}
+	var items []Item
+
+	// create a new query on the kind Person
+	q := datastore.NewQuery("Item").Filter("SuperMarket =", smarket)
+
+	// and finally execute the query retrieving all values into p.
+	_, getErr := q.GetAll(ctx, &items)
+	if getErr != nil {
+		http.Error(w, getErr.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	fmt.Fprintf(w, "Items in %s : %v ", smarket, sum)
+	fmt.Fprintf(w, "Items with supermarket: '%s' : %v ", smarket, items)
 }
