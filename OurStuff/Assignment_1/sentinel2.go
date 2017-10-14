9b39b13a87bc5d36977126994ddd3a7c31a4f403
Kirluu <kirluu4thewin@gmail.com>
2017-10-14 22:41:31 +0200

    Code progressing towards understanding the API results via JSON

--- OurStuff/Assignment_1/sentinel2.go
+++ OurStuff/Assignment_1/sentinel2.go
@@ -11,60 +11,19 @@ import (
 	"io"
 
 	"cloud.google.com/go/bigquery"
-	"cloud.google.com/go/storage"
+	/*"cloud.google.com/go/storage"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
-	"google.golang.org/api/cloudkms/v1"
+	"google.golang.org/api/cloudkms/v1"*/
 	"google.golang.org/api/iterator"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/urlfetch"
 	"strconv"
+	"strings"
 )
 
-func adc() {
-	ctx := context.Background()
-
-	// [START auth_cloud_implicit]
-
-	// For API packages whose import path is starting with "cloud.google.com/go",
-	// such as cloud.google.com/go/storage in this case, if there are no credentials
-	// provided, the client library will look for credentials in the environment.
-	storageClient, err := storage.NewClient(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	it := storageClient.Buckets(ctx, "project-id")
-	for {
-		bucketAttrs, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(bucketAttrs.Name)
-	}
-
-	// For packages whose import path is starting with "google.golang.org/api",
-	// such as google.golang.org/api/cloudkms/v1, use the
-	// golang.org/x/oauth2/google package as shown below.
-	oauthClient, err := google.DefaultClient(ctx, cloudkms.CloudPlatformScope)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	kmsService, err := cloudkms.New(oauthClient)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// [END auth_cloud_implicit]
-	_ = kmsService
-}
-
-func query(lat float64, long float64, w http.ResponseWriter, r *http.Request) (*bigquery.RowIterator, error) {
+func getBaseUrls(lat float64, long float64, w http.ResponseWriter, r *http.Request) ([]string, error) {
 	//ctx := context.Background()
 	ctx := appengine.NewContext(r)
 
@@ -92,47 +51,32 @@ func query(lat float64, long float64, w http.ResponseWriter, r *http.Request) (*
 	// Use standard SQL syntax for queries.
 	// See: https://cloud.google.com/bigquery/sql-reference/
 	query.QueryConfig.UseStandardSQL = true
-	return query.Read(ctx)
+
+	var queryIterator, readErr = query.Read(ctx)
+	if (readErr != nil) {
+		return nil, readErr
+	}
+
+	return printBaseUrls(w, queryIterator)
 }
 
-func printBaseUrls(w io.Writer, iter *bigquery.RowIterator) error {
+func printBaseUrls(w io.Writer, iter *bigquery.RowIterator) ([]string, error) {
+	var resList []string
+
 	for {
 		var baseUrl string
 		err := iter.Next(&baseUrl)
 		if err == iterator.Done {
-			return nil
+			return resList, err
 		}
 		if err != nil {
-			return err
+			return nil, err
 		}
-
+		resList = append(resList, baseUrl)
 		fmt.Fprintf(w, baseUrl)
 	}
-}
 
-func printResults(w io.Writer, iter *bigquery.RowIterator) error {
-	for {
-		var row []bigquery.Value
-		err := iter.Next(&row)
-		if err == iterator.Done {
-			return nil
-		}
-		if err != nil {
-			return err
-		}
-
-		fmt.Fprintln(w, "titles:")
-		ts := row[0].([]bigquery.Value)
-		for _, t := range ts {
-			record := t.([]bigquery.Value)
-			title := record[0].(string)
-			cnt := record[1].(int64)
-			fmt.Fprintf(w, "\t%s: %d\n", title, cnt)
-		}
-
-		words := row[1].(int64)
-		fmt.Fprintf(w, "total unique words: %d\n", words)
-	}
+	return resList, nil
 }
 
 func init() {
@@ -155,7 +99,6 @@ func init() {
 
 
 func main() {
-	//adc()
 	appengine.Main()
 
 
@@ -167,6 +110,18 @@ func main() {
 		log.Fatal(err)
 	}
 	*/
+}
+
+type ApiResult struct {
+	Prefixes []string `json:"prefixes"`
+	Items []ApiItem `json:"items"`
+}
+
+type ApiItem struct {
+	Id string `json:"id"`
+	SelfLink string `json:"selfLink"`
+	Name string `json:"name"`
+	MediaLink string `json:"mediaLink"`
 }
 
 func getBigquery(w http.ResponseWriter, r *http.Request) {
@@ -185,189 +140,61 @@ func getBigquery(w http.ResponseWriter, r *http.Request) {
 		return }
 
 	// Perform query, given the now successfully parsed parameters
-	iter, err := query(lat, long, w, r)
-
-	if err == nil && iter != nil {
-		fmt.Fprintf(w, "Now going to print results!")
-		printBaseUrls(w, iter)
+	baseUrlIter, err := getBaseUrls(lat, long, w, r)
+	if err != nil || baseUrlIter == nil {
+		fmt.Fprintf(w, "BigQuery contact failed %s", err)
 		return
 	}
-	fmt.Fprintf(w, "BigQuery contact failed %s", err)
+	if (len(baseUrlIter) == 0) {
+		fmt.Fprintf(w, "No base-URLs retrieved.")
+		return
+	}
+
+	// Now use first (arbitrary) base-URL to do a nice little request
+	var baseUrl = baseUrlIter[0]
+	var prefixes, apiPrefErr = apiPrefixesRequest(baseUrl)
+    if (apiPrefErr != nil) {
+		fmt.Fprintf(w, "Failed when getting prefixes: %s", apiPrefErr)
+	}
+	fmt.Fprintf(w, "Succeeded in extracting prefixes:")
+	if len(prefixes) > 0 {
+		fmt.Fprintf(w, prefixes[0]) // TODO: Loop over prefixes and print each, or something like that
+	}
+
+	fmt.Fprintf(w, "\nReached the end of the handler!")
 }
 
-func getImages(w http.ResponseWriter, r *http.Request) {
+func apiPrefixesRequest(baseUrl string) ([]string, error) {
+	// remove the useless front-part of the given base-URL + add known GRANULE-directory
+	var baseUrlCorrect = strings.TrimPrefix(baseUrl, "gs://gcp-public-data-sentinel-2/") + "/GRANULE/"
+	var requestUrl string = "https://www.googleapis.com/storage/v1/b/gcp-public-data-sentinel-2/o"
 
-	lat := r.URL.Query().Get("lat")
-	long := r.URL.Query().Get("long")
+	var fullUrl string = requestUrl + "?delimiter=/&prefix=" + baseUrlCorrect
+	// TODO: Build JSON-compliant class to retrieve results through!
+	httpGet(fullUrl) // Prints the content string
 
-	newReq := getUnknownURL(lat, long)
+	var apiRes, apiErr = httpGetApiResult(fullUrl)
+	if (apiErr != nil) {
+		log.Fatal("Failed to get API result.")
+	}
 
-	req, err := http.NewRequest("GET", newReq, nil)
+	return apiRes.Prefixes, nil
+}
 
+func httpGetApiResult(url string) (ApiResult, error) {
+	response, err := http.Get(url)
 	if err != nil {
-		http.Error(w, "Failed ", http.StatusNotFound)
-		return
+		log.Fatal(err)
+		return ApiResult{}, nil
 	}
 
-	dec := json.NewDecoder(req.Body)
-	errDec := dec.Decode(&req)
-
-	if errDec != nil {
-		http.Error(w, "Failed json decode", http.StatusNotFound)
-		return
+	var res ApiResult
+	dec := json.NewDecoder(response.Body)
+	decErr := dec.Decode(&res)
+	if decErr != nil {
+		return ApiResult{}, decErr
 	}
-
-	fmt.Fprintf(w, "lat long %s : %v ", lat, long)
-}
-
-func getUnknownURL(lat string, long string) string {
-
-	return "some url"
-}
-
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "MY MESSAGE - PRINT IT NOOOW!")
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	// first create a new context
-	c := appengine.NewContext(r)
-	// and use that context to create a new http client
-	client := urlfetch.Client(c)
-
-	// now we can use that http client as before
-	res, err := client.Get("http://google.com")
-	if err != nil {
-		http.Error(w, fmt.Sprintf("could not get google: %v", err), http.StatusInternalServerError)
-		return
-	}
-	fmt.Fprintf(w, "Got Google with status %s\n", res.Status)
-}
-
-func completeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Some text!")
-
-	// create a new App Engine context from the HTTP request.
-	ctx := appengine.NewContext(r)
-
-	p := &Item{Name: "gopher", SuperMarket: "Netto"}
-
-	// create a new complete key of kind Person and value gopher.
-	key := datastore.NewKey(ctx, "Person", "gopher", 0, nil)
-	// put p in the datastore.
-	key, err := datastore.Put(ctx, key, p)
-	if err != nil {
-		fmt.Fprintf(w, "Error occurred!")
-		//http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	fmt.Fprintf(w, "gopher stored with key %v", key)
-}
-
-func incompleteHandler(w http.ResponseWriter, r *http.Request) {
-	// create a new App Engine context from the HTTP request.
-	ctx := appengine.NewContext(r)
-
-	p := &Item{Name: "gopher", SuperMarket: "Netto"}
-
-	// create a new complete key of kind Person.
-	key := datastore.NewIncompleteKey(ctx, "Person", nil)
-	// put p in the datastore.
-	key, err := datastore.Put(ctx, key, p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		fmt.Fprintf(w, "Error!")
-		return
-	}
-	fmt.Fprintf(w, "gopher stored with key %v", key)
-}
-
-func getHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := appengine.NewContext(r)
-
-	key := datastore.NewKey(ctx, "Person", "gopher", 0, nil)
-
-	var p Item
-	err := datastore.Get(ctx, key, &p)
-	if err != nil {
-		http.Error(w, "Person not found", http.StatusNotFound)
-		return
-	}
-	fmt.Fprintln(w, p)
-}
-
-var items = []Item{}
-
-type Item struct {
-	Name        string `json:"name"`
-	Price       int    `json:"price"`
-	SuperMarket string `json:"superMarket"`
-}
-
-func addItem(w http.ResponseWriter, r *http.Request) {
-	var i Item
-
-	dec := json.NewDecoder(r.Body)
-	err := dec.Decode(&i)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	items = append(items, i)
-	fmt.Fprintf(w, "Added %v", i.Name)
-}
-
-func remove(s []Item, i int) []Item {
-	s[len(s)-1], s[i] = s[i], s[len(s)-1]
-	return s[:len(s)-1]
-}
-
-func removeItemByName(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "could not read body: %v", err)
-		return
-	}
-	name := string(b)
-
-	for index, e := range items {
-		if e.Name == name {
-			items = remove(items, index)
-		}
-	}
-
-}
-
-func removeAllItems(w http.ResponseWriter, r *http.Request) {
-	items = []Item{}
-}
-
-func getItemsTotalPrice(w http.ResponseWriter, r *http.Request) {
-	sum := 0
-	for _, e := range items {
-		sum += e.Price
-	}
-
-	fmt.Fprintf(w, "total price: %v", sum)
-}
-
-func getItemsInSupermarket(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "could not read body: %v", err)
-		return
-	}
-	smarket := string(b)
-
-	sum := []Item{}
-	for _, e := range items {
-		if e.SuperMarket == smarket {
-			sum = append(sum, e)
-		}
-	}
-
-	fmt.Fprintf(w, "Items in %s : %v ", smarket, sum)
+	return res, nil
 }
 
 func testquery(w http.ResponseWriter, r *http.Request) {
@@ -387,8 +214,7 @@ func testquery(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err2)
 	}
 
-	printResults(w, it)
-
+	printBaseUrls(w, it)
 }
 
 func httpGet(url string) string {
