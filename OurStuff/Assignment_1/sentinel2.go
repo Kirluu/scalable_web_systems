2da444d1e24a023dbeb6c4f4644da9433c2ebafa
archigo <aspo-ja@hotmail.com>
2017-10-17 10:27:55 +0200

    prints

--- OurStuff/Assignment_1/sentinel2.go
+++ OurStuff/Assignment_1/sentinel2.go
@@ -9,81 +9,20 @@ import (
 
 	"io"
 
-	"cloud.google.com/go/bigquery"
-	/*"cloud.google.com/go/storage"
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/cloudkms/v1"*/
 	"strconv"
 	"strings"
+
+	"cloud.google.com/go/bigquery"
 
 	"google.golang.org/api/iterator"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/urlfetch"
 )
 
-func getBaseUrls(lat float64, long float64, w http.ResponseWriter, r *http.Request) ([]string, error) {
-	//ctx := context.Background()
-	ctx := appengine.NewContext(r)
-
-	//client, err := bigquery.NewClient(ctx, "kulr-178408")
-	client, err := bigquery.NewClient(ctx, "johaa-178408")
-	if err != nil {
-		fmt.Fprintf(w, "error when creating BigQuery client from appengine context!")
-		return nil, err
-	}
-
-	latLess := lat - 0.5
-	latMore := lat + 0.5
-	longLess := long - 0.5
-	longMore := long + 0.5
-
-	// Without params:
-	//queryString := "SELECT base_url FROM `bigquery-public-data.cloud_storage_geo_index.sentinel_2_index` where west_lon < 60 and west_lon > 59.5 and south_lat > 80.9 and south_lat < 81 LIMIT 1000"
-	// with params:
-	queryString := fmt.Sprintf("SELECT base_url FROM `bigquery-public-data.cloud_storage_geo_index.sentinel_2_index` where west_lon < %g and west_lon > %g and south_lat < %g and south_lat > %g LIMIT 1000", longMore, longLess, latMore, latLess)
-
-	fmt.Fprintf(w, "Your query: \n"+queryString)
-
-	query := client.Query(queryString)
-
-	// Use standard SQL syntax for queries.
-	// See: https://cloud.google.com/bigquery/sql-reference/
-	query.QueryConfig.UseStandardSQL = true
-
-	var queryIterator, readErr = query.Read(ctx)
-	if readErr != nil {
-		return nil, readErr
-	}
-
-	return printBaseUrls(w, queryIterator)
-}
-
-func printBaseUrls(w io.Writer, iter *bigquery.RowIterator) ([]string, error) {
-	var resList []string
-
-	for {
-		var baseUrl []bigquery.Value
-		err := iter.Next(&baseUrl)
-		if err == iterator.Done {
-			return resList, nil
-		}
-		if err != nil {
-			return nil, err
-		}
-		resList = append(resList, fmt.Sprintf("%s", baseUrl[0]))
-		fmt.Fprintf(w, "\n%s", baseUrl[0])
-	}
-	fmt.Fprintf(w, "\n")
-
-	return resList, nil
-}
-
 func init() {
 	http.HandleFunc("/", handler) // Overall default handler
 	http.HandleFunc("/apitest", getApiTestQuery)
 	http.HandleFunc("/bigquery", getBigquery)
-	http.HandleFunc("/test2", testquery)
 
 }
 
@@ -99,19 +38,8 @@ func main() {
 	}*/
 }
 
-type ApiResult struct {
-	Prefixes []string  `json:"prefixes"`
-	Items    []ApiItem `json:"items"`
-}
-
-type ApiItem struct {
-	Id        string `json:"id"`
-	SelfLink  string `json:"selfLink"`
-	Name      string `json:"name"`
-	MediaLink string `json:"mediaLink"`
-}
-
 func getBigquery(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "getBigQuery:\n")
 	latStr := r.URL.Query().Get("lat")
 	if latStr == "" {
 		fmt.Fprintf(w, "Missing query input: latitude (lat)")
@@ -137,7 +65,7 @@ func getBigquery(w http.ResponseWriter, r *http.Request) {
 	// Perform query, given the now successfully parsed parameters
 	baseUrlIter, err := getBaseUrls(lat, long, w, r)
 	if err != nil || baseUrlIter == nil {
-		fmt.Fprintf(w, "BigQuery contact failed %s", err)
+		fmt.Fprintf(w, "BigQuery contact failed %s\n", err)
 		return
 	}
 	if len(baseUrlIter) == 0 {
@@ -147,42 +75,119 @@ func getBigquery(w http.ResponseWriter, r *http.Request) {
 
 	// Now use first (arbitrary) base-URL to do a nice little request
 	if len(baseUrlIter) == 0 {
-		fmt.Fprintf(w, "No base-URLs could be found for the given Long and Lat arguments.")
+		fmt.Fprintf(w, "No base-URLs could be found for the given Long and Lat arguments.\n")
 		return
 	}
+
 	var baseUrl = baseUrlIter[0]
 	handleBaseUrl(w, r, baseUrl)
 
-	fmt.Fprintf(w, "\nReached the end of the handler!")
+	fmt.Fprintf(w, "\nReached the end of the handler!\n")
+}
+
+func getBaseUrls(lat float64, long float64, w http.ResponseWriter, r *http.Request) ([]string, error) {
+	fmt.Fprintf(w, "getBaseUrls\n")
+	//ctx := context.Background()
+	ctx := appengine.NewContext(r)
+
+	//client, err := bigquery.NewClient(ctx, "kulr-178408")
+	client, err := bigquery.NewClient(ctx, "johaa-178408")
+	if err != nil {
+		fmt.Fprintf(w, "error when creating BigQuery client from appengine context!")
+		return nil, err
+	}
+
+	latLess := lat - 0.5
+	latMore := lat + 0.5
+	longLess := long - 0.5
+	longMore := long + 0.5
+
+	// Without params:
+	//queryString := "SELECT base_url FROM `bigquery-public-data.cloud_storage_geo_index.sentinel_2_index` where west_lon < 60 and west_lon > 59.5 and south_lat > 80.9 and south_lat < 81 LIMIT 1000"
+	// with params:
+	queryString := fmt.Sprintf("SELECT base_url FROM `bigquery-public-data.cloud_storage_geo_index.sentinel_2_index` where west_lon < %g and west_lon > %g and south_lat < %g and south_lat > %g LIMIT 1000", longMore, longLess, latMore, latLess)
+
+	fmt.Fprintf(w, "Your query: \n"+queryString+"\n\n")
+
+	query := client.Query(queryString)
+
+	// Use standard SQL syntax for queries.
+	// See: https://cloud.google.com/bigquery/sql-reference/
+	query.QueryConfig.UseStandardSQL = true
+
+	var queryIterator, readErr = query.Read(ctx)
+	if readErr != nil {
+		return nil, readErr
+	}
+
+	fmt.Fprintf(w, "\n\n")
+
+	return printBaseUrls(w, queryIterator)
+}
+
+func printBaseUrls(w io.Writer, iter *bigquery.RowIterator) ([]string, error) {
+	fmt.Fprintf(w, "printBaseUrls\n")
+	var resList []string
+
+	for {
+		var baseUrl []bigquery.Value
+		err := iter.Next(&baseUrl)
+		if err == iterator.Done {
+			return resList, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		resList = append(resList, fmt.Sprintf("%s", baseUrl[0]))
+		fmt.Fprintf(w, "\n%s", baseUrl[0])
+	}
+	fmt.Fprintf(w, "\n\n")
+
+	return resList, nil
+}
+
+type ApiResult struct {
+	Prefixes []string  `json:"prefixes"`
+	Items    []ApiItem `json:"items"`
+}
+
+type ApiItem struct {
+	Id        string `json:"id"`
+	SelfLink  string `json:"selfLink"`
+	Name      string `json:"name"`
+	MediaLink string `json:"mediaLink"`
 }
 
 func getApiTestQuery(w http.ResponseWriter, r *http.Request) {
 	// Test the api access giving a specific base-URL
 
 	str, err := httpGet(w, r, "https://www.googleapis.com/storage/v1/b/gcp-public-data-sentinel-2/o?delimiter=/&prefix=tiles/39/P/YT/S2A_MSIL1C_20170921T064621_N0205_R020_T39PYT_20170921T065933.SAFE/GRANULE/")
-	if (err != nil) {
+	if err != nil {
 		fmt.Fprintf(w, "Error! %s", err)
 	}
 	fmt.Fprintf(w, "CONTENTS:\n\n%s", str)
 	//handleBaseUrl(w, "gs://gcp-public-data-sentinel-2/tiles/41/X/MK/S2A_MSIL1C_20170810T110621_N0205_R137_T41XMK_20170810T110621.SAFE")
 }
 
-func handleBaseUrl(w http.ResponseWriter, r *http.Request,  baseUrl string) error {
+func handleBaseUrl(w http.ResponseWriter, r *http.Request, baseUrl string) error {
+	fmt.Fprintf(w, "handleBaseUrl\n")
 
 	var prefixes, apiPrefErr = apiPrefixesRequest(w, r, baseUrl)
 	if apiPrefErr != nil {
-		fmt.Fprintf(w, "Failed when getting prefixes: %s", apiPrefErr)
+		fmt.Fprintf(w, "Failed when getting prefixes: %s\n", apiPrefErr)
 	}
-	fmt.Fprintf(w, "Succeeded in extracting prefixes:")
+	fmt.Fprintf(w, "Succeeded in extracting prefixes:\n")
 	if len(prefixes) > 0 {
-		fmt.Fprintf(w, prefixes[0]) // TODO: Loop over prefixes and print each, or something like that
+		fmt.Fprintf(w, prefixes[0]+"\n") // TODO: Loop over prefixes and print each, or something like that
 	}
+	fmt.Fprintf(w, "\n\n")
 
 	// Full success, return no error:
 	return nil
 }
 
 func apiPrefixesRequest(w http.ResponseWriter, r *http.Request, baseUrl string) ([]string, error) {
+	fmt.Fprintf(w, "apiPrefixRequest\n")
 	// remove the useless front-part of the given base-URL + add known GRANULE-directory
 	var baseUrlCorrect = strings.TrimPrefix(baseUrl, "gs://gcp-public-data-sentinel-2/") + "/GRANULE/"
 	var requestUrl string = "https://www.googleapis.com/storage/v1/b/gcp-public-data-sentinel-2/o"
@@ -196,6 +201,8 @@ func apiPrefixesRequest(w http.ResponseWriter, r *http.Request, baseUrl string) 
 		log.Fatal("Failed to get API result.")
 		return []string{}, apiErr
 	}
+
+	fmt.Fprintf(w, "\n\n")
 
 	return apiRes.Prefixes, nil
 }
@@ -217,32 +224,8 @@ func httpGetApiResult(w http.ResponseWriter, r *http.Request, url string) (ApiRe
 	}
 
 	fmt.Fprintf(w, "ApiResult given Base-URL request:\n%s\n", res)
-	
+
 	return res, nil
-}
-
-func testquery(w http.ResponseWriter, r *http.Request) {
-	ctx := appengine.NewContext(r)
-	//client, err := bigquery.NewClient(ctx, "johaa-178408")
-	client, err := bigquery.NewClient(ctx, "kulr-178408")
-	if err != nil {
-	}
-	//q := client.Query("SELECT * FROM `bigquery-public-data.cloud_storage_geo_index.sentinel_2_index` where west_lon < 60 and west_lon > 59.5 and south_lat > 80.9 and south_lat < 81 LIMIT 1000")
-	q := client.Query("SELECT base_url FROM `bigquery-public-data.cloud_storage_geo_index.sentinel_2_index` where west_lon < 60 and west_lon > 59.5 and south_lat > 80.9 and south_lat < 81 LIMIT 1000")
-	q.QueryConfig.UseStandardSQL = true
-	//q.DefaultProjectID = "bigquery-public-data"
-	//q.DefaultDatasetID = "cloud_storage_geo_index"
-
-	it, err2 := q.Read(ctx)
-
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-
-	//fmt.Fprintln(w, it)
-	//printResults(w, it)
-
-	printBaseUrls(w, it)
 }
 
 func httpGet(w http.ResponseWriter, r *http.Request, url string) (string, error) {
@@ -279,4 +262,3 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Got Google with status %s\n", res.Status)
 }
-
