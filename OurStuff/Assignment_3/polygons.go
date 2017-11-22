303fcefa394355b1717d4729ec9b87446dfdd852
archigo <aspo-ja@hotmail.com>
2017-11-22 10:46:41 +0100

    countImages

--- OurStuff/Assignment_3/polygons.go
+++ OurStuff/Assignment_3/polygons.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"math"
-	"github.com/golang/geo/s2"
 	"net/http"
+	"strconv"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/golang/geo/s2"
+	"google.golang.org/api/iterator"
 )
 
 func searchCountry(client *http.Client, country string) {
 
 	// TODO: Retrieve PSLG stuff
-
 
 }
 
@@ -52,4 +56,62 @@ func handlePolygon(thingy [][2]float64) {
 		totalArea = totalArea + c.RectBound().Area()
 	}
 	fmt.Printf("Total Area with multiple rectangles: %v", totalArea)
+}
+
+func countImages(ctx context.Context, rectangles [][4]float64, time1 string, time2 string) (int, error) {
+
+	client, err := bigquery.NewClient(ctx, "johaa-178408")
+	if err != nil {
+		//fmt.Fprintf(w, "error when creating BigQuery client from appengine context!")
+		return -1, err
+	}
+
+	queryString := "SELECT COUNT(base_url) FROM `bigquery-public-data.cloud_storage_geo_index.sentinel_2_index` where"
+
+	first := true
+
+	for _, rect := range rectangles {
+		if !first {
+			queryString += " or ("
+		} else {
+			queryString += " ( "
+			first = false
+		}
+		lat1 := rect[0]
+		lat2 := rect[1]
+		long1 := rect[2]
+		long2 := rect[3]
+
+		queryString += fmt.Sprintf("north_lat > %g and west_lon > %g and south_lat < %g and east_lon < %g )", lat1, long1, lat2, long2)
+
+	}
+
+	if time1 != "" && time2 != "" {
+		queryString += " and (generation_time > " + time1 + " and generation_time < " + time2 + " )"
+	}
+
+	query := client.Query(queryString)
+
+	// Use standard SQL syntax for queries.
+	// See: https://cloud.google.com/bigquery/sql-reference/
+	query.QueryConfig.UseStandardSQL = true
+
+	var queryIterator, readErr = query.Read(ctx)
+	if readErr != nil {
+		return -1, readErr
+	}
+
+	var count []bigquery.Value
+	errI := queryIterator.Next(&count)
+	if errI == iterator.Done {
+		if i, err := strconv.Atoi(fmt.Sprintf("%s", count)); err == nil {
+			return i, nil
+		}
+		return -1, nil
+
+	}
+	if errI != nil {
+		return -1, err
+	}
+	return -1, nil
 }
